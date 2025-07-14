e68881861293da177ee53608d780a21d8ad03274
Ritik Nagpure <80842165+Ritik-Nagpure@users.noreply.github.com>
2025-07-14 20:09:05 +0530

    initialization

---
+++ src/Layout/App/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      Started
+      <p className='text-red-500'>Hello World</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Layout/Header/Header.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import logo from "../../../public/extras/vastrum-logo.png";
+
+function Header() {
+  return (
+    <div className="bg-gray-800 text-white p-4 flex justify-between items-center">
+      Header Component
+    </div>
+  );
+}
+
+export default Header;
